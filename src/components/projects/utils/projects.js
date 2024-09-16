2f5eb2250593cf05337baf05ae2fe2887e9a3d6b
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-16 13:06:56 -0300

    Add Finanzas app image to projects

--- src/components/projects/utils/projects.js
+++ src/components/projects/utils/projects.js
@@ -1,12 +1,18 @@
 import l5Image from '../../../assets/l5.jpg'
 import gayatry from '../../../assets/gayatry.jpg'
 import dillo from '../../../assets/dillo.jpg'
+import finanzas from '../../../assets/finanzas-app.png'
 
 export const projects = [
   {
     title: "Dillostores landing page",
     href: "https://dillostores.com/",
     img: dillo,
+  },
+  {
+    title: "Finanzas app",
+    href: "https://finanzas-arg.vercel.app/",
+    img: finanzas,
   },
   {
     title: "Lujan en 5' (newspaper)",
